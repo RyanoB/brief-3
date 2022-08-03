7a44373137072a48f4006eb0703fab0a3cb23d64
DevSoleo <devsoleo@protonmail.com>
2022-08-03 14:24:21 +0200

    Ajout d'arguments au lancement du script

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 import json
 import subprocess
+import sys
 
 import infrastructure
 import jenkins_installer
 
-infrastructure.run()
-print("wqeqeqwe qwje jlkqwej qwjlek jqwlke lkq IOKKKKK")
-jenkins_installer.run()
+if sys.argv[1] == "--infrastrucure-only":
+    infrastructure.run()
+elif sys.argv[1] == "--service-only":
+    jenkins_installer.run()
+else:
+    infrastructure.run()
+    jenkins_installer.run()
